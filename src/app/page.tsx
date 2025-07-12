ebf7cad01f19f63b48ca3d2ea36c3a0365728cf9
Oscar Sanchez <78560380+OscarSanchez1214@users.noreply.github.com>
2025-07-11 22:47:51 -0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { VerifyButton } from "@/components/VerifyButton";
 
 export default function Page() {
